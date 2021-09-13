eadb9e1130122b5581cb7ed73589fe44c37a3bf2
George ******* <90574418+SolGeorge@users.noreply.github.com>
2021-09-13 23:36:29 +0300

    Update Swap_main.c

--- Swap/Src/Swap_main.c
+++ Swap/Src/Swap_main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 
-#include "header.h"
+#include "Swap_functions.h"
+
+
+int verificare(int a)
+{
+    printf("\nIntrodu un numar: ");
+
+    if(scanf("%d", &a) != 1 )
+    {   
+        printf("\nNu ai introdus un numar!\nMai incearca!");
+            
+        fflush(stdin);
+            
+        return verificare(a);   
+    }
+    else
+    {   
+    	printf("\nNumarul introdus este: %d.",a);
+            
+    	return 0;	
+    }
+    
+}
 
 int main()
 {
-    int x = 2, y = 3;
-
+    int x , y ;
+    
+    verificare(x);
+    verificare(y);
+        
     permanent_swap(&x, &y);
 
     swap(x, y);
