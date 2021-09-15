850c3541303bd09b1dc14a9d9dfc44673391ed4c
George ******* <90574418+SolGeorge@users.noreply.github.com>
2021-09-16 00:01:08 +0300

    Add files via upload

--- Swap/Src/Swap_function.c
+++ Swap/Src/Swap_function.c
@@ -3,25 +3,64 @@
 #include "Swap_function.h"
  
 
- void swap(int a, int b)
+ void swap_view(int number_1, int number_2)
  {
-     int p;
+    int number_temp;
 
-     p= a;
-     a= b;
-     b= p;
+    number_temp = number_1;
+    number_1    = number_2;
+    number_2    = number_temp;
 
-     printf("\n%d\t%d", a , b);
+     printf("\n%d\t%d", number_1, number_2);
  }
 
- void permanent_swap(int *a, int *b)
+ void swap_pointer(int *number_1, int *number_2)
  {
-     int p;
+    int number_temp;
 
-      p= *a;
-     *a= *b;
-     *b=  p;
+     number_temp = *number_1;
+    *number_1    = *number_2;
+    *number_2    =  number_temp;
 
-	 printf("\n%d\t%d", *a , *b);
+	 printf("\n%d\t%d", *number_1, *number_2);
+ }
+ void swap_XOR(int *number_1, int *number_2)
+ {
+    *number_1 = *number_1^ *number_2;
+    *number_2 = *number_1^ *number_2;
+    *number_1 = *number_1^ *number_2;
+
+     printf("\n%d\t%d", *number_1, *number_2);
  }
 
+int verificare_int(int verifey)
+{
+    printf("\nIntrodu un numar: ");
+
+    while(scanf("%d", &verifey) != 1 )
+    {   
+        printf("\nNu ai introdus un numar!");
+        printf("\nMai incearca!");    
+        fflush(stdin);
+        printf("\nIntrodu un numar: ");      
+    }
+  
+    printf("\nNumarul introdus este: %d.\n",verifey);
+ 	
+    return verifey;
+}
+int verificare_char(char simbol)
+{
+    printf("\nIntrodu un simbol: ");
+
+    fflush(stdin);
+
+    while(scanf("%c", &simbol) != 1 )
+    {   
+        printf("\nNu ai introdus un simbol!");
+        printf("\nMai incearca!");    
+        printf("\nIntrodu un simbol: ");
+        fflush(stdin);   
+    }
+    return simbol;
+}
--- Swap/Src/Swap_main.c
+++ Swap/Src/Swap_main.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-#include "Swap_functions.h"
+#include "Swap_function.h"
 
-
-int verificare(int a)
-{
-    printf("\nIntrodu un numar: ");
-
-    if(scanf("%d", &a) != 1 )
-    {   
-        printf("\nNu ai introdus un numar!");
-        printf("\nMai incearca!");    
-        fflush(stdin);
-            
-        return verificare(a);   
-    }
-    else
-    {   
-    	printf("\nNumarul introdus este: %d.\n",a);
-            
-    	return 0;	
-    }
-    
-}
 
 int main()
 {
-    int x , y ;
+    int numarul_1 , numarul_2 ;
     
-    verificare(x);
-    verificare(y);
-        
-    permanent_swap(&x, &y);
+    numarul_1= verificare_int(numarul_1);
+    numarul_2= verificare_int(numarul_2);
+    
+	printf("\nNumerele introduse sunt: %d si %d",numarul_1 ,numarul_2);  
 
-    swap(x, y);
+    char simbol;
 
+    mai_incearca:
 
+    printf("\nPentru a face un swap prebuie sa introduci un simbol!");
+    printf("\nIntrodu \"#\" pentru swap_view!");
+    printf("\nIntrodu \"$\" pentru swap_pointer!");
+    printf("\nIntrodu \"&\" pentru swap_XOR!");
+    
+    simbol= verificare_char(simbol);
+
+    switch(simbol)
+    {
+    case '#': 
+    swap_view(numarul_1, numarul_2);
+    break;
+
+    case '$': 
+    swap_pointer(&numarul_1, &numarul_2);
+    break;
+
+    case '&':
+    swap_XOR(&numarul_1, &numarul_2);
+    break;
+    
+    default:
+    printf("Nu ai introdus un simbol valid!");
+    }
+    
+    goto mai_incearca;
     return 0;
 }
